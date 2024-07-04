6ebe1dd9d53de24e3f492faf39604f9c41f4e82f
Ioana Ilie <ilieioana97@gmail.com>
2024-07-04 13:54:21 +0300

    Displaying platform icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,10 +2,17 @@ import { useState, useEffect } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
+export interface Platform {
+  id: number,
+  name: string,
+  slug: string
+}
+
 export interface Game {
   id: number;
   name: string;
-  background_image: string;
+  background_image: string,
+  parent_platforms: {platform: Platform}[] // array of obj where each obj has a property platform of type platform
 }
 
 interface FetchGamesResponse {
