9c3727edc3d3661231d005e718231f9003d0eecd
Ioana Ilie <ilieioana97@gmail.com>
2024-07-31 10:53:14 +0300

    Refactoring - simplify game query

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -20,16 +20,17 @@ const useGames = (gameQuery: GameQuery) =>
     queryFn: ({ pageParam }) =>
       apiClient.getAll({
         params: {
-          genres: gameQuery.genre?.id,
-          parent_platforms: gameQuery.platform?.id,
+          genres: gameQuery.genreId,
+          parent_platforms: gameQuery.platformId,
           ordering: gameQuery.sortOrder,
           search: gameQuery.searchText,
-          page: pageParam
+          page: pageParam,
         },
       }),
-      initialPageParam: 1,
-      getNextPageParam: (lastPage, allPages) => lastPage.next ? allPages.length + 1 : undefined,
-      staleTime: 24 * 60 * 60 * 1000 // 24h
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, allPages) =>
+      lastPage.next ? allPages.length + 1 : undefined,
+    staleTime: 24 * 60 * 60 * 1000, // 24h
   });
 
 export default useGames;
